3e531ca73628926122a006fe381c410c0c166fe5
Lutfiddin <72572875+lutfiddin-ux@users.noreply.github.com>
2021-06-14 15:10:53 +0500

    search method

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -5,17 +5,24 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\Product;
 use App\Models\Banner;
+use App\Models\Category;
 
 class MainController extends Controller
 {
     public function index() {
         $products = Product::inRandomOrder()->limit(3)->get();
+        $categories = Category::inRandomOrder()->limit(3)->get();
         $banners = Banner::all();
-        return view('main', compact('products', 'banners'));
+        return view('main', compact('products', 'banners', 'categories'));
     }
 
     public function showProduct($id) {
         $product = Product::find($id);
         return view('product', compact('product'));
+    }
+
+    public function search(Request $req) {
+        $data = Product::where('name', 'like', '%' . $req->input('search') . '%')->get();
+    	return view('search', compact('data'));
     }
 }
